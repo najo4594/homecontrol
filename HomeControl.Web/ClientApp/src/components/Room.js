ac69722889e5a32be34eb4ad6a60fe61d7f8624b
Matthias Gohsen <matthias.gohsen@six-group.com>
2021-01-07 16:17:27 +0100

    Render all devices of bedroom

--- HomeControl.Web/ClientApp/src/components/Room.js
+++ HomeControl.Web/ClientApp/src/components/Room.js
@@ -1,31 +1,43 @@
 ﻿import React, {Component} from 'react';
 import {Card, Col, Row} from 'react-bootstrap'
-import {useParams, withRouter} from "react-router-dom";
+import {withRouter} from "react-router-dom";
 
 class Room extends Component {
 
     constructor(props) {
         super(props);
-         
+
         let roomId = null;
-        if(Number.isInteger(+this.props.match.params.roomId)){
+        if (Number.isInteger(+this.props.match.params.roomId)) {
             roomId = parseInt(this.props.match.params.roomId);
-        }        
-        
+        }
+
         this.state = {
-            isLoading: true,
+            loading: true,
             roomId: roomId,
             devices: null
         };
     }
 
-    componentDidMount() {                   
+    componentDidMount() {
         this.getDevices();
     }
 
     render() {
         return (
-            <h1>{this.state.roomId}</h1>
+            this.state.loading ? <p>Loading...</p> :
+                <Row>
+                    {this.state.devices.map(device =>
+                        <Col xs={3} className="my-3">
+                            <Card>
+                                <Card.Header>{device.name}</Card.Header>
+                                <Card.Body>
+                                    Some information about the device
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )}
+                </Row>
         );
     }
 
