45684eabd1749e75d1c3d38cebb553a39fba4425
Matthias Gohsen <matthias.gohsen@six-group.com>
2021-01-09 13:45:18 +0100

    Remove loading text

--- HomeControl.Web/ClientApp/src/components/Room.js
+++ HomeControl.Web/ClientApp/src/components/Room.js
@@ -55,7 +55,7 @@ class Room extends Component {
 
     render() {
         if (this.state.loading) {
-            return <p>Loading...</p>
+            return null;
         } else if (this.state.error) {
             return <p>Error happened :(</p>
         } else {
