7ca1cfc47ff13b3ffb68c383f5220711e50e0c3e
Matthias Gohsen <matthias.gohsen@six-group.com>
2021-01-16 22:23:57 +0100

    Add test for SynchronizationController

---
+++ HomeControl.Test/Controllers/SynchronizationControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using FakeItEasy;
+using HomeControl.Service.Services.Interfaces;
+using HomeControl.Web.Controllers;
+using NUnit.Framework;
+
+namespace HomeControl.Test.Controllers
+{
+	public class SynchronizationControllerTest
+	{
+		private ISynchronizationService _synchronizationService;
+		private SynchronizationController _synchronizationController;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_synchronizationService = A.Fake<ISynchronizationService>(f => f.Strict());
+			
+			_synchronizationController = new SynchronizationController(_synchronizationService);
+
+			A.CallTo(() => _synchronizationService.Synchronize()).DoesNothing();
+		}
+		
+		[Test]
+		public void Synchronize()
+		{
+			// Act
+			_synchronizationController.Synchronize();
+			
+			// Assert
+			A.CallTo(() => _synchronizationService.Synchronize()).MustHaveHappenedOnceExactly();
+		}
+	}
+}
